cc2d9f27794e9647327aaaed84ac0ee1a35ce23f
Gloria-kuang <chumeikuang@Chumeis-MacBook-Air.local>
2022-04-21 16:26:00 -0400

    created action-creators

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import {useSelector} from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 
 function App() {
-  const account = useSelector ( (state) => state.account);
-  console.log(account)
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
+  const AC = bindActionCreators(actionCreators, dispatch);
+
+  console.log(AC);
+  return <div className="App"></div>;
 }
 
 export default App;
